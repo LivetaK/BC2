64107d8ce6d2d78481442889eda239a0773a8d71
Liveta <livetakaval@gmail.com>
2024-10-20 21:49:03 +0300

    - prideta nauja block klase

--- class.hpp
+++ class.hpp
@@ -41,47 +41,113 @@ class user {
 		double getBalance() {
 			return balance;
 		}
+	user() = default;
 
 };
 
 class transaction {
 
-private:
-	string transaction_id = " ";
-	string sender = " "; // sintejo viesasis raktas
-	string recipient = " "; // gavejo viesasis raktas
-	double amount = 0.0;
-public:
-	void setTransactionId(string tran) {
-		transaction_id = tran;
-	};
+	private:
+		string transaction_id = " ";
+		string sender = " "; // sintejo viesasis raktas
+		string recipient = " "; // gavejo viesasis raktas
+		double amount = 0.0;
+	public:
+		void setTransactionId(string tran) {
+			transaction_id = tran;
+		};
 
-	string getTransactionId() {
-		return transaction_id;
-	}
+		string getTransactionId() {
+			return transaction_id;
+		}
 
-	void setSender(string Sender) {
-		sender = Sender;
-	}
+		void setSender(string Sender) {
+			sender = Sender;
+		}
 
-	string getSender() {
-		return sender;
-	}
+		string getSender() {
+			return sender;
+		}
 
-	void setRecipient(string Recipient) {
-		recipient = Recipient;
-	}
+		void setRecipient(string Recipient) {
+			recipient = Recipient;
+		}
 
-	string getRecipient() {
-		return recipient;
-	}
+		string getRecipient() {
+			return recipient;
+		}
 
-	void setAmount(double Amount) {
-		amount = Amount;
-	}
+		void setAmount(double Amount) {
+			amount = Amount;
+		}
 
-	double getAmount() {
-		return amount;
-	}
+		double getAmount() {
+			return amount;
+		}
+	transaction() = default;
+};
 
+
+
+class block {
+	private: 
+		string previousBlockHash = " ";
+		time_t timestamp = time(0);
+		vector<transaction> Transactions;
+		string blockHash;
+	public: 
+
+
+		void setPBH(string PBH) {
+			previousBlockHash = PBH;
+		}
+		string getPBH() {
+			return previousBlockHash;
+		}
+
+
+
+		void setTimestamp(time_t time) {
+			timestamp = time;
+		}
+		time_t getTimestamp() {
+			return timestamp;
+		}
+
+
+
+		void setTransactions(vector<transaction> tran) { // visas vektorius
+			Transactions = tran;
+		}
+		vector<transaction> getTransactions() {
+			return Transactions;
+		}
+
+
+
+		void addTransaction(transaction tran) {
+			Transactions.push_back(tran);
+		}
+		transaction getTransaction(int i) {
+			return Transactions[i];
+		}
+
+
+		string calculateBlockHash() {
+			string data = previousBlockHash + to_string(timestamp);
+			for (int i = 0; i < Transactions.size(); i++) {
+				data += Transactions[i].getTransactionId();
+			}
+			return hashfun(data);
+		}
+
+		string getBlockHash() {
+			return blockHash;
+		}
+
+		block(string PBH) {
+			previousBlockHash = PBH;
+			timestamp = time(0);
+			blockHash = calculateBlockHash();
+		}
 };
